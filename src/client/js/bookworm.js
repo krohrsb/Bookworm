5ce3b216e8f1a20ef99cba179f1af5e862a3a280
Kyle Brown <blackbarn@gmail.com>
2014-02-03 15:57:09 -0800

    removing restangular hack due to fix.

--- src/client/js/bookworm.js
+++ src/client/js/bookworm.js
@@ -42,10 +42,6 @@
         // Define Request Interceptor
         Restangular.setRequestInterceptor(function (element, operation, model) {
             ngProgressLite.start();
-            if (_.isObject(element) && element['0']) {
-                console.warn('Restangular not fixed yet, turning array back into array');
-                element = _.toArray(element);
-            }
 
             if (operation.toLowerCase() === 'put' && (model === 'books' || model === 'authors')) {
                 if (_.isArray(element)) {
